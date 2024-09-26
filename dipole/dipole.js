b957997741445c0a235a7e5640ebd3739072d3c1
Owen Buchheit <obuchheit45@gmail.com>
2024-09-25 22:35:24 -0500

    added functions and style to dipole page

--- dipole/dipole.js
+++ dipole/dipole.js
@@ -1,7 +1,20 @@
+document.querySelector('#hertzSelector').addEventListener('change',hertzUnit)
+
+
 document.querySelector('#calcButton').addEventListener('click', calculate);
 
+
 let denom = 468;
+let base = 1000000;
+let frequency = 0;
+
+
 
 function calculate() {
   let answer = denom/freq
+  document.querySelector('.output').innerText = answer
+}
+
+function hertzUnit() {
+  if (this.innerText)
 }
