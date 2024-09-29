30514e390b394ec3508783340ae9d4bb941f4219
Owen Buchheit <obuchheit45@gmail.com>
2024-09-28 20:34:11 -0500

    Started feet inches decimal function.

--- dipole/dipole.js
+++ dipole/dipole.js
@@ -13,7 +13,6 @@ document.querySelector('.lenUnit').addEventListener('click', calculate);
 
 
 //Adds and calculates freq with every keystroke.
-//Need to debug backspace down to 0.
 
 document.addEventListener('keydown', (event) => {
 
@@ -44,9 +43,16 @@ function calculate() {
     int = currentNum
     frequency = base * int;
     answer = denom/frequency;
+    if (document.querySelector('.lenUnit').innerText == 'Metric') {
+      let feet = Math.floor(answer);
+      let remainder = answer - Math.floor(answer);
+      remainder = remainder * 12;
+      let inches = Math.floor(remainder)
+      remainder = remainder - Math.floor(remainder) 
+      let decimal = 32 * remainder
+    }
   }
-  console.log(currentNum, answer)
-
+  
   document.querySelector('.antLength').innerText = answer;
   document.querySelector('.mes').innerText = answer/2;
   document.querySelector('.mes1').innerText = answer/2;
