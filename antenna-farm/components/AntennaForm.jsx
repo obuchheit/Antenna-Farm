9a9a52ef2585ad9b67c2b48acc43301011503c26
obuchheit <obuchheit45@gmail.com>
2024-11-12 22:23:55 -0600

    Add form component

---
+++ antenna-farm/components/AntennaForm.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react"
+
+function AntennaForm() {
+    <>
+        <div className="panel">
+            
+        </div>
+    </>
+}
+
+export default AntennaForm
